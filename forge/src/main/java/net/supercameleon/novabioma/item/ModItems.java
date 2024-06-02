c5f4f4b5fada9a3818a833e8fbe20d3f43edf84f
SuperCameleon <126679563+SuperCameleon@users.noreply.github.com>
2024-06-02 22:14:33 +0200

    Added burn (peat clod)

--- forge/src/main/java/net/supercameleon/novabioma/item/ModItems.java
+++ forge/src/main/java/net/supercameleon/novabioma/item/ModItems.java
@@ -6,13 +6,14 @@ import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 import net.supercameleon.novabioma.NovaBioma;
+import net.supercameleon.novabioma.item.custom.FuelItem;
 
 public class ModItems {
     public static final DeferredRegister<Item> ITEMS =
             DeferredRegister.create(ForgeRegistries.ITEMS, NovaBioma.MOD_ID);
 
     public static final RegistryObject<Item> PEATCLOD = ITEMS.register("peat_clod",
-            () -> new Item(new Item.Properties()));
+            () -> new FuelItem(new Item.Properties(), 600));
 
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
